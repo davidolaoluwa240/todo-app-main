c468ddbe2c66cd73134a1387889771d01740d40a
davidolaoluwa240 <davidolaoluwa240@gmail.com>
2022-07-12 20:15:34 -0300

    refactor theming localstorage support logic

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -3,16 +3,29 @@ const rootElem = $("[data-theme]");
 const switchThemeElem = $(".todo__info-icon", true);
 
 // Internal State
-let theme = localStorage.getItem("theme") || "light";
+let theme;
+
+const updateThemingAttribute = function (value) {
+  rootElem.setAttribute("data-theme", value);
+};
 
 const toggleTheme = function () {
   // Toggle the theme variable value
   theme = theme === "light" ? "dark" : "light";
-  // Update the rootElem data-theme value
-  rootElem.setAttribute("data-theme", theme);
   // Update theme in the localstorage
   DB.updateUserTheme(theme);
+  // Update the rootElem data-theme value
+  updateThemingAttribute(theme);
 };
+
+// Init
+(() => {
+  // Set theme value in load
+  theme = localStorage.getItem("theme") || "light";
+
+  // Invoke toggle theme
+  updateThemingAttribute(theme);
+})();
 
 // Event Listener
 switchThemeElem.forEach((element) =>
